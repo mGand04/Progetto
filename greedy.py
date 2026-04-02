7493f28ddccfb48e2c5a838b5c6562efd9e5cb08
Federico Uricchio <federico.g.uricchio@gmail.com>
2026-04-02 12:29:35 +0200

    Ciao

--- greedy.py
+++ greedy.py
@@ -10,14 +10,14 @@ fold = input("Inserisci il nome della cartella(n25/50/100): ")
 file_name = input("Inserisci il nome del file(es.C101.txt): ")
 
 # Percorso del file 
-path_base = r'C:\Users\mgand\OneDrive\Desktop\Ottimizzazzione_sr\Progetto\Istanze'
+path_base = r'C:\Users\safet\OneDrive\Desktop\Progetto\Istanze'
 
 path = os.path.join(path_base, fold, file_name)
 
 # Lettura dell'header del file
 try:
     veichle_info = np.genfromtxt(path, skip_header=4, max_rows=1)
-
+    
     veichle_quantity = veichle_info[0]
     veichle_capacity = veichle_info[1]
     print("Veichle info")
@@ -27,13 +27,15 @@ try:
     data = np.genfromtxt(path, skip_header=9)
 
     cols = {'ID': 0, 'X': 1, 'Y': 2, 'DEM': 3, 'READY': 4, 'DUE': 5, 'SERV': 6}
-
-    print(data[:2])
+    demand = data[:, 3]
+    print(demand)
+    #print(data[:2])
 
     # Matrice delle distanze
 
     distances = np.zeros((data.shape[0], data.shape[0]))
 
+    
     # print(distances)
     for i in range(data.shape[0]):
         for j in range(data.shape[0]):
@@ -46,22 +48,41 @@ try:
 
     # Algoritmo 1:
     print("Algoritmo 1")
-
+    n_nodi = len(distances)
+    visitati = np.zeros(n_nodi, dtype=bool)
+    visitati[0] = True  # Il magazzino è già "visitato" (punto di partenza)
+    percorsi_totali = []
     # Ciclo su ogni veicolo per assegnare il path
-    for i in range(veichle_quantity): 
+    for i in range(int(veichle_quantity)): 
+        if np.all(visitati):break #se tutti i clienti sono serviti ci fermiamo
         y = 0   # Tempo di inizio del percorso
+        percorso_attuale = [0]
         capacity = veichle_capacity # Merce iniale di ogni veicolo
         actual_node = 0 # Parto dal magazzino
 
         # Scrittura del path
-        while capacity >= 0:
-            
-            # Calcolo delle distanze minime dal nodo attuale
-            index = np.argmin(distances[actual_node])
-            
-
-
-
+        while True:
+            # Creiamo una copia temporanea delle distanze dal nodo attuale
+            distanze_temporanee = np.array(distances[actual_node],dtype=float)
+            # Escludiamo i nodi già visitati o troppo pesanti impostando la loro distanza a infinito
+            for j in range(n_nodi):
+                if visitati[j] or demand[j] > capacity:
+                    distanze_temporanee[j] = np.inf
+            prossimo_nodo = int(np.argmin(distanze_temporanee))
+            # Se np.argmin restituisce un nodo con distanza infinita, significa che:
+        # 1. Non ci sono più nodi da visitare
+        # 2. La capacità residua non permette di servire altri nodi
+            if distanze_temporanee[prossimo_nodo] == np.inf:
+                percorso_attuale.append(0) # Torna al magazzino
+                break
+            # Aggiornamento stato
+            visitati[prossimo_nodo] = True
+            capacity -= demand[prossimo_nodo]
+            percorso_attuale.append(prossimo_nodo)
+            actual_node = prossimo_nodo
+        percorsi_totali.append(percorso_attuale)
+    for idx, p in enumerate(percorsi_totali):
+        print(f"Veicolo {idx+1}: {p}")
 except FileNotFoundError:
     print(f"ERRORE: Il file '{file_name}' non esiste nella cartella '{fold}'.")
     print("Controlla di aver scritto correttamente i nomi.")
